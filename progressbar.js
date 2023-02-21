0b7b740d35d20f47039c424cc600fdb62024e2bb
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-20 23:54:08 -0300

    Update progressbar.js

--- progressbar.js
+++ progressbar.js
@@ -1,52 +1,40 @@
 const buttonStartStop = document.getElementById('startDayCode');
 const progressToday = document.getElementById('progressToday');
 
+// progressToday.style.width = `${progress}%`;
+
 let stopWatch = false;
 let width = 0;
-const changeProgress = (progress) => {
-  if (progress < 101) {
-    progressToday.style.width = `${progress}%`;
-
-  } else {
-    stop()
-  }
-};
 
 buttonStartStop.addEventListener('click', function () {
-    stopWatch === false ? start() : stop();
+    stopWatch ? stop() : start();
     console.log(stopWatch)
 })
+
+var incremento = () => width++
 
 function start() {
     stopWatch = true
     buttonStartStop.classList.replace('btn-outline-success', 'btn-outline-danger')
     buttonStartStop.innerText = "stop day code"
-    progressWidth(stopWatch)
     
+    setTimeout(() => {
+      console.log('incremento');
+      incremento()
+    }, 100);
 }
+
 
 function stop() {
     stopWatch = false
     buttonStartStop.classList.replace('btn-outline-danger' ,'btn-outline-success')
     buttonStartStop.innerText = "start day code"
-    progressWidth(stopWatch)
     
 }
 
-function progressWidth(stopWatch) {  
-  if (stopWatch === true) {
-    // width++
-    // setInterval(timerProgress, 10)
-    
-  } else {
-    // clearInterval()
-  }
-  
-}
 
 
-function timerProgress() {
-  width++
-  changeProgress(width)
 
-}
+
+// faça o incremento ++ x milesegundos
+// adicione esse incremento ao width até chegar em 100%
